dcc4a0af4ffe2f6e134948850129c30b7a163c9b
Alder Maven <43149194+AlderMaven@users.noreply.github.com>
2019-05-20 15:36:44 -0500

    Revised RNG.h and added randString prototype
    Added functionality to RNG.h using chrono for a theoretically more diverse set of numbers in a shorter time, however, this seems to be working incorrectly as can be seen in randString's output of only one type of character per string.

--- Headers and Programs/RNG.h
+++ Headers and Programs/RNG.h
@@ -2,8 +2,10 @@
 //Simple class to contain RNG functions
 #include<ctime>
 #include<stdlib.h>
+#include <chrono>
 
 class RNG{
+	public:
     int srandNum(int min, int max){
         if(min < 0|| max<0 || min>max){ //Error cases
             return -1;
@@ -35,5 +37,24 @@ class RNG{
 	    //Since that difference is > 0, this gives a range that can be returned reliably in consistant time
 	
 	    return result;
-    }	
+    }
+	int fastRandNum(int min, int max){
+		using namespace std::chrono;
+
+		if(min < 0|| max<0 || min>max){ //Error cases
+		    return -1;
+	    }
+	    if(min == max){ //Quick return
+		    return min;
+	    }
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		int result;
+		int temp = max+1-min;
+		int tempTwo = high_resolution_clock::now().time_since_epoch().count();
+		srand(tempTwo);
+		
+		
+	    result = (rand()%(temp))+min;
+		return result;
+	}
 };
--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
 Basic RNGs made w/ c++
 RNG.h has a containing class for all the currently implemented functions, while the individual functions can be found in their respective headers within the sub-folder
+RNG.h and randString.cpp require c++ 11 for compilation
 -BB
